f3be22d542415107ce6e5a654b76baf3e16aeed1
Nikul <Nikul@192.168.1.8>
2016-09-12 23:41:56 +1000

    In the Loan class, getBorrower() method has been tested by GetBorrowerTest class's method testGetBorrower(). Test passed 100%

--- test/library/entities/GetBorrowerTest.java
+++ test/library/entities/GetBorrowerTest.java
@@ -5,6 +5,9 @@
  */
 package library.entities;
 
+import java.util.Date;
+import library.interfaces.entities.IBook;
+import library.interfaces.entities.IMember;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -14,17 +17,30 @@ import static org.junit.Assert.*;
  * @author Nikul
  */
 public class GetBorrowerTest {
-    
-    public GetBorrowerTest() {
-    }
+     
+    Loan instance;
+    IBook book;
+    IMember member;
+    Date borrowDate, returnDate;
     
     @Before
     public void setUp() {
+        book = new Book("Author1","ITC515","0055455",12345);
+        member = new Member("James", "Mack", "046977777", "email1", 123);
+        borrowDate = new Date(2016,05,10);
+        returnDate = new Date(2016, 07, 10);
+        
+        instance = new Loan(book, member, borrowDate, returnDate);
     }
 
     // TODO add test methods here.
     // The methods must be annotated with annotation @Test. For example:
     //
-    // @Test
-    // public void hello() {}
+    @Test
+    public void testGetBorrower() {
+        IMember expectedResult = member;
+        IMember result = instance.getBorrower();
+        //System.out.println(".... " + expectedResult );
+        assertEquals(expectedResult, result);
+    }
 }
