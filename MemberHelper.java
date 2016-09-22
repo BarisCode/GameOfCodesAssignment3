fb8b22b6b2dd324c3ab8bdd011a8dbdecc8537d3
BarisPro <bmovoisin@gmail.com>
2016-09-22 10:34:11 +1000

    Comment makeMember
    Comment makeMember

--- MemberHelper.java
+++ MemberHelper.java
@@ -4,11 +4,11 @@ import library.entities.Member;
 import library.interfaces.daos.IMemberHelper;
 import library.interfaces.entities.IMember;
 
-public class MemberHelper implements IMemberHelper {
+public class MemberHelper implements IMemberHelper { 
 
 	@Override
 	public IMember makeMember(String firstName, String lastName,
-			String contactPhone, String emailAddress, int id) {
+			String contactPhone, String emailAddress, int id) { //create a new member
 		
 		return new Member(firstName, lastName, contactPhone, emailAddress, id);
 	}
