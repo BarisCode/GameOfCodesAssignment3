3b599bbd5e62798c40c576d538ecc2fc5559e275
Nikul <Nikul@192.168.1.8>
2016-09-22 13:14:01 +1000

    Created the LoanTest class to do low level integration testing for Loan class.

---
+++ test/library/entities/LoanTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library.entities;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author Nikul
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({library.entities.GetIDTest.class, library.entities.GetBorrowerTest.class, library.entities.GetBookTest.class, library.entities.CompleteTest.class, library.entities.GetStateTest.class, library.entities.IsOverDueTest.class, library.entities.CheckOverDueTest.class})
+public class LoanTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+}
