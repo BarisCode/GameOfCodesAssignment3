2753b9977bf4e9c9debf67c21d4f878deef55611
Nikul Chaudhary <NikulChaudhary@users.noreply.github.com>
2016-09-23 00:05:27 +1000

    Commented on the  LoanTest class

--- test/library/entities/LoanTest.java
+++ test/library/entities/LoanTest.java
@@ -13,7 +13,11 @@ import org.junit.runners.Suite;
  *
  * @author Nikul
  */
+ 
+ // Run with suite class
 @RunWith(Suite.class)
+
+// All test cases of the Loan class are added to suitecase to test a Loan entity.
 @Suite.SuiteClasses({library.entities.GetIDTest.class, library.entities.GetBorrowerTest.class, library.entities.GetBookTest.class, library.entities.CompleteTest.class, library.entities.GetStateTest.class, library.entities.IsOverDueTest.class, library.entities.CheckOverDueTest.class})
 public class LoanTest {
 
